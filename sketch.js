5d936e13124629c9f290f63536c984bbed668571
Anwar Endris <anwarendo@gmail.com>
2022-01-27 14:17:58 +0300

    black board added

--- sketch.js
+++ sketch.js
@@ -1,13 +1,23 @@
 function setup() {
-  noCanvas();
-  // createCanvas(400,400);
+  // noCanvas(window.innerWidth, window.innerHeight);
+  var canv = createCanvas(700, 400);
+  canv.parent("canvas-container");
+  canv.class("w3-black w3-card-4 w3-margin");
+  canv.elt.style.border = "8px solid orange";
 }
 
 function draw() {
+  noStroke();
+  var rad;
   if(mouseIsPressed) {
-    fill(0);
-  } else {
+    frameRate(60);
     fill(255);
+    rad = 10;
+  } else {
+    frameRate(30);
+    fill(random(100),random(255), random(100), random(30));
+    rad = random(10, 100);
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  
+  ellipse(mouseX, mouseY, rad, rad);
 }
