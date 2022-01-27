c744376d8f8814ad1f48afef225d6c9a50159d68
Anwar Endris <anwarendo@gmail.com>
2022-01-27 16:34:10 +0300

    board manipulation done

--- sketch.js
+++ sketch.js
@@ -1,23 +1,84 @@
+var dots = [];
+var i = 0;
+var x = 0;
+var y = 0;
+var read = false;
+var cleanBoard = false;
+var write = false;
+
 function setup() {
   // noCanvas(window.innerWidth, window.innerHeight);
   var canv = createCanvas(700, 400);
   canv.parent("canvas-container");
   canv.class("w3-black w3-card-4 w3-margin");
   canv.elt.style.border = "8px solid orange";
+    
 }
 
 function draw() {
+  console.log('read: ', read);
+  console.log('write: ', write);
+  console.log('cleanBoard', cleanBoard);
+  if(i > 3600) { // 20 minuts worth frame count
+    i = 0;
+  }
   noStroke();
   var rad;
   if(mouseIsPressed) {
     frameRate(60);
     fill(255);
     rad = 10;
+    var point = {'x': mouseX, 'y': mouseY};
+    if(write) {
+      dots.push(point);
+    } 
+    
   } else {
     frameRate(30);
     fill(random(100),random(255), random(100), random(30));
     rad = random(10, 100);
+    
   }
-  
-  ellipse(mouseX, mouseY, rad, rad);
+  x = mouseX;
+  y = mouseY;
+  ellipse(x, y, rad, rad);
+  if(read) {
+    i++;
+    if(i >= dots.length){
+      i = 0;
+      background(0);
+    }
+    // console.log(dots.length);
+    fill(0,255,255);
+    rad = 10;
+    if(dots !== null && i < dots.length) {
+      x = dots[i].x + 100;
+      y = dots[i].y + 20;
+      console.log(dots[i].x);
+      rect(x, y, rad, rad);
+    }   
+    
+  }
+
+  if(cleanBoard) {
+    background(0);
+    cleanBoard = false;
+  }
+
+}
+
+function keyPressed() {
+  if(keyCode === LEFT_ARROW) {
+    read = !read;
+  }
+  if(keyCode === RIGHT_ARROW) {
+    cleanBoard = !cleanBoard;
+  }
+
+  if(keyCode === UP_ARROW) {
+    write = !write;
+  }
+  if(keyCode === DOWN_ARROW) {
+    dots = [];
+  }
 }
